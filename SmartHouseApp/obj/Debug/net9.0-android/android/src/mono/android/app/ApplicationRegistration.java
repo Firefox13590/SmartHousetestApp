03839d343e536eee752fa1c468140edf63ba7495
Firefox13590 <pharlyb@gmail.com>
2025-04-03 12:11:16 -0400

    added buttons for data sender and receiver

---
+++ SmartHouseApp/obj/Debug/net9.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("SmartHouseApp.MainApplication, SmartHouseApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64a27c28f98da1dad3.MainApplication.class, crc64a27c28f98da1dad3.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
--- SmartHouseApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/SmartHouseApp.AssemblyInfo.cs
+++ SmartHouseApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/SmartHouseApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartHouseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+103fd30594cec4a7a59e50efa2bd65ad84c97d87")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartHouseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartHouseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
